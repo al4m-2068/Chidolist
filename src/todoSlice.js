ab2c2fa05f553317e739318c58e61e2967dc1255
Albar Malik <albar2806@student.abudzar.sch.id>
2026-04-13 13:44:38 +0700

    lupa

--- src/todoSlice.js
+++ src/todoSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const todoSlice = createSlice({
     name: 'chidolist',
-    initialState: ['Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', 'Welcome', ],
+    initialState: [],
     reducers: {
         tambahTugas: (state, action) => {
             state.push(action.payload)
